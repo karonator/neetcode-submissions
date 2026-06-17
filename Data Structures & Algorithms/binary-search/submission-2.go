5e52dc54ebf3ac9f351efa217e7cdeb0ecdc6338
Karen Grigorian <5732864+karonator@users.noreply.github.com>
2026-06-17 23:42:01 +0200

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.go
@@ -0,0 +1,18 @@
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left <= right {
+		mid := left + (right - left) / 2
+
+		if target == nums[mid] {
+			return mid
+		}
+		if target > nums[mid] {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return -1
+}
