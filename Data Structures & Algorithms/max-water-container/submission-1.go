fd53aa08bf7002a848865483522591dcd438838c
Karen Grigorian <5732864+karonator@users.noreply.github.com>
2026-06-22 23:49:58 +0200

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.go
@@ -0,0 +1,35 @@
+func abs (a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
+
+func min(a int, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func maxArea(heights []int) int {
+	ans := 0
+
+	i := 0
+	j := len(heights) - 1
+
+	for i < j {
+		water := min(heights[i], heights[j]) * (j - i)
+		if water > ans {
+			ans = water
+		}
+
+		if heights[i] < heights[j] {
+			i++
+		} else {
+			j--
+		}
+	}
+
+	return ans
+}
