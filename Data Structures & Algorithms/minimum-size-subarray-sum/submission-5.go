f4ea471ff719119d2fae4f813a4d3093d7fcbdf7
Karen Grigorian <5732864+karonator@users.noreply.github.com>
2026-08-30 01:05:00 +0200

    Add: minimum-size-subarray-sum - submission-5

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-5.go
@@ -0,0 +1,22 @@
+
+func minSubArrayLen(target int, nums []int) int {
+	ans := 0
+	left := 0
+	sum := 0
+
+	for right := 0; right < len(nums); right++ {
+		sum += nums[right]
+		if sum >= target {
+			for sum - nums[left] >= target {
+				sum -= nums[left]
+				left++
+			}
+			if ans > 0 {
+				ans = min(ans, right - left + 1)
+			} else {
+				ans = right - left + 1
+			}
+		}
+	}
+	return ans
+}
