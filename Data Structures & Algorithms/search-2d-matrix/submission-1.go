9f85f395b8861543c86ef804b045e674fadfb359
Karen Grigorian <5732864+karonator@users.noreply.github.com>
2026-09-01 23:55:34 +0200

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.go
@@ -0,0 +1,25 @@
+func searchMatrix(matrix [][]int, target int) bool {
+	rows := len(matrix)
+	cols := len(matrix[0])
+
+	lo, hi := 0, rows * cols - 1
+	for lo < hi {
+		mid := lo + (hi - lo) / 2
+		row := mid / cols
+		col := mid % cols
+
+		if target <= matrix[row][col]  {
+			hi = mid
+		} else {
+			lo = mid + 1
+		}
+	}
+	row := lo / cols
+	col := lo % cols
+
+	if matrix[row][col] == target {
+		return true
+	}
+
+	return false
+}
