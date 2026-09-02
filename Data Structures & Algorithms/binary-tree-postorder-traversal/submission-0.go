f13e8fb4b009306e584445f33bb6fa89ba2feb5c
Karen Grigorian <5732864+karonator@users.noreply.github.com>
2026-09-02 22:58:13 +0200

    Add: binary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-0.go
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ 
+func postorderTraversal(root *TreeNode) []int {
+    ans := make([]int, 0)
+
+	type Item struct {
+		Node 	*TreeNode
+		Visited bool
+	}
+
+	stack := make([]Item, 0)
+	stack = append(stack, Item{
+		Node: root,
+		Visited: false,
+	})
+
+	for len(stack) > 0 {
+		elem := stack[len(stack) - 1]
+		stack = stack[:len(stack) - 1]
+
+		if elem.Node == nil {
+			continue
+		}
+
+		if elem.Visited {
+			ans = append(ans, elem.Node.Val)
+		} else {
+			stack = append(stack, Item{
+				Node: elem.Node,
+				Visited: true,
+			})
+			stack = append(stack, Item{
+				Node: elem.Node.Right,
+				Visited: false,
+			})
+			stack = append(stack, Item{
+				Node: elem.Node.Left,
+				Visited: false,
+			})
+		}
+	}
+
+	return ans
+}
+
