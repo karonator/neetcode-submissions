4149fdeb4674a54060886976294de6113fe60917
Karen Grigorian <5732864+karonator@users.noreply.github.com>
2026-09-04 00:52:01 +0200

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.go
@@ -0,0 +1,18 @@
+func validPalindromeStrict(s string) bool {
+	fmt.Println(s)
+	for i := 0; i < len(s) / 2; i++ {
+		if s[i] != s[len(s) - i - 1] {
+			return false
+		}
+	}
+	return true
+}
+
+func validPalindrome(s string) bool {
+	for i := 0; i < len(s) / 2; i++ {
+		if s[i] != s[len(s) - i - 1] {
+			return validPalindromeStrict(s[i: len(s) - i - 1]) || validPalindromeStrict(s[i + 1: len(s) - i])
+		}
+	}
+	return true
+}
