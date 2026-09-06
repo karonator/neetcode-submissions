31218f54b46b1cc9590c8a395dcfc74701e9085a
Karen Grigorian <5732864+karonator@users.noreply.github.com>
2026-09-06 03:34:38 +0200

    Add: max-area-of-island - submission-2

---
+++ Data Structures & Algorithms/max-area-of-island/submission-2.go
@@ -0,0 +1,36 @@
+func maxAreaOfIsland(grid [][]int) int {
+    fill := func(x int, y int) int {
+		stack := [][]int{{x, y}}
+		area := 0
+		dirs := [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+
+		for len(stack) > 0 {
+			point := stack[len(stack) - 1]
+			stack = stack[:len(stack) - 1]
+
+			if grid[point[0]][point[1]] == 1 {
+				area++
+				grid[point[0]][point[1]] = 2
+				for _, dir := range dirs {
+					x := point[0] + dir[0]
+					y := point[1] + dir[1]
+					if x >= 0 && y >= 0 && x < len(grid) && y < len(grid[0]) && grid[x][y] == 1 {
+						stack = append(stack, []int{x, y})
+					}
+				}
+			}
+		}
+		return area
+	}
+
+	maxArea := 0
+	for	i := range(grid) {
+		for j := range(grid[0]) {
+			if grid[i][j] == 1 {
+				maxArea = max(maxArea, fill(i, j))
+			}
+		}
+	}
+
+	return maxArea
+}
