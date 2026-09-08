a0168f6a7f6fc22a71abc2968cc85839d4b421a1
Karen Grigorian <5732864+karonator@users.noreply.github.com>
2026-09-09 00:32:44 +0200

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.go
@@ -0,0 +1,11 @@
+func rob(nums []int) int {
+	data := make([]int, len(nums))
+	data[0] = nums[0]
+	if len(nums) > 1 {
+		data[1] = max(data[0], nums[1])
+	}
+	for i := 2; i < len(nums); i++ {
+		data[i] = max(data[i - 1], data[i - 2] + nums[i])
+	}
+	return data[len(nums) - 1]
+}
