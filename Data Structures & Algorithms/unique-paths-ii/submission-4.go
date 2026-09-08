a5c1ed9c19d6da813b59dd6d9b99969968159b0f
Karen Grigorian <5732864+karonator@users.noreply.github.com>
2026-09-08 23:13:33 +0200

    Add: unique-paths-ii - submission-4

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-4.go
@@ -0,0 +1,27 @@
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	data := make([][]int, len(obstacleGrid))
+	for i := range obstacleGrid {
+		data[i] = make([]int, len(obstacleGrid[0]))
+	}
+
+	for i := range obstacleGrid {
+		for j := range obstacleGrid[0] {
+			if obstacleGrid[i][j] == 1 {
+				continue
+			}
+	
+			if i == 0 || j == 0 {
+				if i > 0 {
+					data[i][j] = data[i - 1][j]
+				} else if j > 0 {
+					data[i][j] = data[i][j - 1]
+				} else {
+					data[0][0] = 1
+				}
+			} else {
+				data[i][j] = data[i - 1][j] + data[i][j - 1]
+			}
+		}
+	}
+	return data[len(data) - 1][len(data[0]) - 1]
+}
