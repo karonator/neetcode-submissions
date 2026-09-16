3710e5b4a9523fb2afe1864a9442359de3d82d58
Karen Grigorian <5732864+karonator@users.noreply.github.com>
2026-09-16 12:17:08 +0200

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.go
@@ -0,0 +1,30 @@
+func longestConsecutive(nums []int) int {
+	exists := make(map[int]struct{})
+	for _, num := range nums {
+		exists[num] = struct{}{}
+	}
+
+	starts := make([]int, 0)
+	for num := range exists {
+		if _, found := exists[num - 1]; !found {
+			starts = append(starts, num)
+		}
+	}
+	fmt.Println(starts)
+
+	maxLen := 0
+	for _, start := range starts {
+		curLen := 1
+		cur := start
+		for {
+			if _, found := exists[cur + 1]; !found {
+				break
+			}
+			cur++
+			curLen++
+		}
+		maxLen = max(maxLen, curLen)
+	}
+
+	return maxLen
+}
