39cf676f1b288fa3fa97e7f1854c8f0b003ccd7c
Karen Grigorian <5732864+karonator@users.noreply.github.com>
2026-09-17 15:29:56 +0200

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.go
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type MinHeap []*ListNode
+
+func (h MinHeap) Len() int           { return len(h) }
+func (h MinHeap) Less(i, j int) bool { return h[i].Val < h[j].Val }
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MinHeap) Push(x any) {
+	*h = append(*h, x.(*ListNode))
+}
+
+func (h *MinHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	minHeap := MinHeap{}
+
+	for _, listHead := range lists {
+		if listHead != nil {
+			minHeap = append(minHeap, listHead)
+		}
+	}
+
+	heap.Init(&minHeap)
+
+	dummyHead := &ListNode{}
+	tail := dummyHead
+
+	for minHeap.Len() != 0 {
+		node := heap.Pop(&minHeap).(*ListNode)
+
+		next := node.Next
+		tail.Next = node
+		tail = tail.Next
+
+		if next != nil {
+			heap.Push(&minHeap, next)
+		}
+	}
+	tail.Next = nil
+
+	return dummyHead.Next
+}
