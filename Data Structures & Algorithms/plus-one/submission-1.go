a5f79b03927632206478409fb846e0ac21100b10
Karen Grigorian <5732864+karonator@users.noreply.github.com>
2026-09-20 19:25:38 +0200

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.go
@@ -0,0 +1,17 @@
+func plusOne(digits []int) []int {
+	transfer := false
+	for i := len(digits) - 1; i >= 0; i-- {
+		digits[i] += 1
+		if digits[i] >= 10 {
+			digits[i] = digits[i] % 10
+			transfer = true
+		} else {
+			transfer = false
+			break
+		}
+	}
+	if transfer {
+		return append([]int{1}, digits...)
+	}
+	return digits
+}
