550d65ace204a04084525915c969b7555ee6f1b1
Karen Grigorian <5732864+karonator@users.noreply.github.com>
2026-09-21 22:30:54 +0200

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.go
@@ -0,0 +1,37 @@
+func countComponents(n int, edges [][]int) int {
+    nodes := make(map[int][]int)
+	for _, edge := range edges {
+		if _, found := nodes[edge[0]]; !found {
+			nodes[edge[0]] = []int{edge[1]}
+		} else {
+			nodes[edge[0]] = append(nodes[edge[0]], edge[1])
+		}
+		if _, found := nodes[edge[1]]; !found {
+			nodes[edge[1]] = []int{edge[0]}
+		} else {
+			nodes[edge[1]] = append(nodes[edge[1]], edge[0])
+		}
+	}
+
+	visited := make(map[int]struct{})
+	ans := 0
+
+	for i := range n {
+		if _, found := visited[i]; found {
+			continue
+		}
+		ans++
+		queue := []int{i}
+		for len(queue) > 0 {
+			node := queue[len(queue) - 1]
+			queue = queue[:len(queue) - 1]
+			if _, found := visited[node]; found {
+				continue
+			}
+			visited[node] = struct{}{}
+			queue = append(queue, nodes[node]...)
+		}
+	}
+
+	return ans
+}
